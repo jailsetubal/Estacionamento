8b02366df6177877a5f8a35e0c7572463d5b304d
jailsetubal <jail.setubal@outlook.com>
2025-09-16 11:32:49 -0300

    Criação da classe Veiculo e suas camadas, Repository, Application e Facade

---
+++ src/main/java/applications/VeiculoApplication.java
@@ -0,0 +1,36 @@
+package applications;
+
+import entities.Veiculo;
+import repositories.VeiculoRepository;
+
+import java.io.IOException;
+import java.util.List;
+
+public class VeiculoApplication {
+    private VeiculoRepository veiculoRepository;
+
+    public VeiculoApplication(VeiculoApplication veiculoApplication){
+        this.veiculoApplication = veiculoApplication;
+    }
+
+    public List<Veiculo> buscarTodos() throws IOException {
+        return this.veiculoRepository.buscarTodos();
+    }
+
+    public Veiculo buscarPorId(Veiculo veiculo) throws IOException{
+        return this.veiculoRepository.buscarPorId(veiculo.getId());
+    }
+
+    public void adicionar(Veiculo veiculo) throws IOException{
+        this.veiculoRepository.adicionar(veiculo);
+    }
+
+    public void remover(Veiculo veiculo) throws IOException{
+        this.veiculoRepository.remover(veiculo.getId());
+    }
+
+    public void atualizar(Veiculo veiculo, int id) throws IOException{
+        this.veiculoRepository.atualizar(veiculo.getId(), veiculo);
+    }
+}
+
